598a062a66861e04cda27be943705e582e4a6d5b
ankur-bohra <ankurbohra04@gmail.com>
2021-10-30 08:58:49 +0530

    Add new bot

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
-from typing import Union
+import os
+import pickle
+import pytz
+import shutil
+from io import BytesIO
+from typing import Union, IO, Dict, List
 from discord.ext.commands.core import check
 import jury
 import discord
-from discord import Embed
 from discord.ext import commands
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "ODg2MjUwMDIyNDcwMDQ1NzA2.YTy2pw.wqb5lFhu7O8GkG-YL8iAoHjB5MM"
 # Don't use my token, use yours!
 
 bot = commands.Bot(command_prefix="$", case_insensitive=True)
@@ -15,12 +19,16 @@ def check_valid_question_number_string(content: str) -> bool:
     return content.isdigit() and 0 < int(content) <= len(jury.questions)
 
 def check_valid_question_number_message(message: discord.Message) -> bool:
+    if message.author.id == bot.user.id:
+        return False
     return check_valid_question_number_string(message.content)
 
 def check_valid_language_string(content: str) -> bool:
     return content in ("cpp", "c++", "py", "python")
 
 def check_valid_language_message(message: discord.Message) -> bool:
+    if message.author.id == bot.user.id:
+        return False
     return check_valid_language_string(message.content)
 
 def check_valid_codeblock_string(content: str) -> bool:
@@ -32,7 +40,29 @@ def check_valid_codeblock_string(content: str) -> bool:
         return not lines[0].startswith("```") or lines[-1].endswith("```")
 
 def check_valid_codeblock_message(message: discord.Message) -> bool:
-    return check_valid_codeblock_string(message.content)
+    if message.author.id == bot.user.id:
+        return False
+    if len(message.attachments) == 0:
+        return check_valid_codeblock_string(message.content)
+    else:
+        return True
+
+def check_code_is_same(team: str, code: str, question_number: int) -> bool:
+    judgement = jury.get_judgement(team, question_number)
+    return judgement and judgement["code"].strip() == code.strip()
+
+def get_identifier(user) -> str:
+    roles = user.roles
+    identifier = ""
+    for role in roles:
+        if role.name.lower() in ('lotus valley', 'dav public school', 'heritage school', 'gd goenka', 'shalom hills', 'kunskapsskolan', 'dps'):
+            identifier = role.name
+            break
+    else:
+        identifier = str(user.id)
+    return identifier
+
+format_time = lambda object: object.strftime('%I:%M:%S %p')
 
 @bot.event
 async def on_ready():
@@ -40,91 +70,92 @@ async def on_ready():
 
 @bot.command()
 async def summary(ctx: commands.Context) -> None:
-    '''Lists user's submissions and their recorded times.
+    '''Lists user's submissions and their recorded points.
     '''
-    message = ""
-    user_id = ctx.message.author.id
-    total_time = 0
+    embed = discord.Embed(title=f"{get_identifier(ctx.author)} Summary", description="These points include penalties, and may change with time.", color=0xc0c0c0)
+    questions_string = ""
+    points_string = ""
+    total_points = 0
     for q_no in range(len(jury.questions)):
         question = jury.questions[q_no]
-        if question['level'] != jury.questions[q_no-1]['level']:
-            message += f"\n\n**{question['level']}:**\n*Non-submission multiplier = x{question['multiplier']}*"
-        message += f"\nQ{q_no+1}. {question['name']}: "  # -1 for 0 indexed
-        judgement = jury.get_judgement(user_id, q_no)
-        if judgement:
-            message += f"{judgement.time_taken} ms"
-            total_time += judgement.time_taken
+        judgement = jury.get_judgement(get_identifier(ctx.message.author), q_no)
+        if not (judgement and judgement["passed"]):
+            points = jury.get_penalised_points(q_no)
         else:
-            message += f" Not submitted"
-    message += f"\n\n\n**Total time** (w/o penalties): {total_time} ms"
-    await ctx.message.channel.send(message)
+            points = judgement["points"]
+        points = round(points, 2)
+        questions_string += f"\nQ{q_no+1}. {question['name']}"
+        points_string += f"\n{points} points"  # +1 to convert to 1 indexed
+        if judgement and judgement["passed"]:
+            formatted_time = format_time(judgement["submission_time"])
+            points_string += f" ({judgement['execution_time']}ms @ {formatted_time})"
+        else:
+            points_string += f" (penalised)"
+        total_points += points
+
+    embed.add_field(name="Question", value=questions_string, inline=True)
+    embed.add_field(name="Points", value=points_string, inline=True)
+    embed.add_field(name="Total points", value=str(total_points) + " points", inline=False)
+    embed.set_footer(
+            text="Requested by " + ctx.author.display_name + " (" + get_identifier(ctx.author) + ")", icon_url = ctx.author.avatar_url)
+    await ctx.message.channel.send(embed=embed)
             
 @bot.command()
 async def leaderboard(ctx: commands.Context) -> None:
     '''Shows a true leaderboard (penalty-aware) of the participants
     '''
     leaderboard = jury.get_leaderboard()
-    message = ""
     if len(leaderboard) == 0:
         await ctx.channel.send("No submissions yet.")
     else:
+        message = ""
         for rank in range(len(leaderboard)):
-            p_data = leaderboard[rank]
-            message = "\n"
+            message += "\n"
             if rank == 0:
                 message += "🥇"
-            elif rank == 2:
+            elif rank == 1:
                 message += "🥈"
-            elif rank == 3:
+            elif rank == 2:
                 message += "🥉"
             else:
-                message = f"  {rank+1}."
-            message += "  "  # Arbitrary padding
-            message += f"<@{p_data['id']}>: {p_data['time']}ms"
-        await ctx.channel.send(message)
+                message += f"  {rank+1}."
+            t = leaderboard[rank]
+            message += "  "  # Some padding
+            message += f"{t['team'] + (' **(You)**' if t['team'] == get_identifier(ctx.message.author) else '')}: {t['points']} points"
+
+        embed = discord.Embed(title="Leaderboard", description=message, color=0x3ad5eb)
+        embed.set_footer(
+            text="Requested by " + ctx.author.display_name + " (" + get_identifier(ctx.author) + ")", icon_url = ctx.author.avatar_url)
+        await ctx.channel.send(embed=embed)
 
 @bot.command()
-async def diff(ctx: commands.Context, question_number: str=None) -> None:
-    '''Shows a unified diff between output and answer for a submitted question.
-
-    Args:
-        question_number: The question to view the diff for. Defaults to the last submitted question.
-    '''
-    if not check_valid_question_number_string(question_number):
-        question_number = await bot.wait_for("message", check=check_valid_question_number_message)
-        question_number = question_number.content
-    question_number = int(question_number.lower().lstrip("q"))
-    # Change question number to 0 indexed
-    question_number -= 1
-    judgement = jury.get_judgement(ctx.message.author.id, question_number)
-    if not judgement:
-        await ctx.channel.send(f"No submission found for question {question_number + 1} ({jury.questions[question_number]['name']}).")  # Back to 1 indexed
-    else:
-        await ctx.channel.send(f"Diff\n```diff\n{judgement.diff.unified}\n```") 
-
-@bot.command()
-async def submit(ctx: commands.Context, question_number: str=None, language: str=None) -> None:
+async def submit(ctx: commands.Context, question_number: str=None, language: str=None, *, code: Union[str, IO]=None) -> None:
     f'''Initiates a prompt to submit code for a question.
     
     Args:
         question_number: A valid question number. Expected format: q1, 2, q3, etc.
         language: The language the submission is in. Must be either "cpp", "c++", "py" or "python".
+        code: The content of the code or a file for the submission.
     '''
+    # QUESTION NUMBER
     if question_number is None or not check_valid_question_number_string(question_number):
         await ctx.channel.send("What question are you submitting? (q1, 2, q3, etc.)")
         question_number = await bot.wait_for("message", check=check_valid_question_number_message)
         question_number = question_number.content
     question_number = int(question_number.lstrip('q'))
 
+    # PROGRAMMING LANGUAGE
     if language is None or not check_valid_language_string(language):
         await ctx.channel.send("What language is your submission in? (\"cpp\", \"c++\", \"py\", \"python\".)")
         language = await bot.wait_for("message", check=check_valid_language_message)
         language = language.content
     language = "cpp-clang" if language.lower() in ("c++", "cpp") else "python3"
-
     
-    await ctx.channel.send(f'''Enter your code for Q{question_number} {jury.questions[question_number-1]['name']} (to be run on {language}).
-Send your code in a codeblock:
+    # CODE
+
+    if len(ctx.message.attachments) == 0 and (not code or not check_valid_codeblock_string(code)) :
+        await ctx.channel.send(f'''Enter your code for Q{question_number} {jury.questions[question_number-1]['name']} (to be run on {language}).
+Send your code in a codeblock (remember to keep the backticks on a separate line):
 Input:
 \```
 print("Hello, World!")
@@ -133,9 +164,28 @@ Output:
 ```
 print("Hello, World!")
 ```
-Do not keep your code and the backticks on the same line.''')
-    code = await bot.wait_for("message", check=check_valid_codeblock_message)
-    code = code.content
+OR upload your file.''')
+        code = await bot.wait_for("message", check=check_valid_codeblock_message)
+        print(str(code))
+        if len(code.attachments) == 0:
+            code = code.content
+        else:
+            try:
+                code = await code.attachments.pop().read().decode("cp1250")
+            except e:
+                await ctx.channel.send(f"Whoa! The bot ran into an error: Unable to read file content: {e}, please try again.")
+                return
+    elif len(ctx.message.attachments) > 0:
+        try:
+            code = await ctx.message.attachments.pop().read()
+            code = code.decode("cp1250")
+        except BaseException as e:
+            await ctx.channel.send(f"Whoa! The bot ran into an error: Unable to read file content: {e}, please try again.")
+            return
+
+    if check_code_is_same(get_identifier(ctx.message.author), code, question_number):
+        await ctx.channel.send(f"You have already submitted this same code. Please do not add unnecessary load on the bot.")
+        return
     source_lines = code.split("\n")
     if code.startswith("```"):
         source_lines = source_lines[1:-1]  # Remove codeblock markdown
@@ -143,38 +193,128 @@ Do not keep your code and the backticks on the same line.''')
 
     # Change question to 0 indexed
     question_number -= 1
-    await ctx.channel.send("Your code is being judged.")
+    judgement_message = await ctx.channel.send("Your code is being judged.")
 
-    async with ctx.typing():
-        judgement = jury.judge(question_number, language, source_code)
-        # OUTPUT
-        message = ""
-        if judgement.passed:
-            message += "All Correct \✔️"
-            message += f"\n\n**Time Considered:** {float(judgement.time_taken)} ms"
-        else:   
-            message += f"\❌ Wrong Answer on line {judgement.diff.flod}"  # First line of difference
-            message += f"\n\nInput\n```\n{judgement.question['input']}\n```"
-            message += f"\n\nOutput\n```\n{judgement.output}\n```"
-            message += f"\n\nAnswer\n```\n{judgement.question['answer']}\n```"
-            message += f"\n\n**Time Considered:** N.A."
-            message += f"\nUse the `$diff question_number` command to view a unified view of the output and the answer."
+    submission_time = ctx.message.created_at
+    # Tell the naive object that it's utc
+    submission_time = submission_time.replace(tzinfo=pytz.timezone("UTC"))
+    # Convert it to ist
+    submission_time = submission_time.astimezone(pytz.timezone('Asia/Kolkata'))
+    judgement = jury.judge(question_number, language, source_code, submission_time)
+    jury.log(get_identifier(ctx.message.author), judgement)
+    # OUTPUT
+    question_string = f"Q{question_number+1}. {jury.questions[judgement['question_number']]['name']}"
+    if judgement["passed"]:
+        embed = discord.Embed(color=0x14b90b)
+        embed.set_author(name="All Correct ✅")
+        embed.add_field(name="Execution Time", value=str(judgement["execution_time"]) + " ms", inline=True)
+        embed.add_field(name="Submission Time", value=format_time(judgement["submission_time"]), inline=True)
+        embed.add_field(name="Submission Points", value=judgement["points"], inline=False)
+    else:
+        error = f"Wrong answer on line {judgement['diff']['lod']}: Expected **\"{judgement['diff']['correct']}\"**, got **\"{judgement['diff']['wrong']}\"**"
+        error += f"\nUse `$tests {question_number+1}` to view this test's input, answer, and your output"
+        embed = discord.Embed(color=0xf03a17, description=error)
+        embed.set_author(name="Test Failed ❌")
+    embed.set_footer(
+            text="Requested by " + ctx.author.display_name + " (" + get_identifier(ctx.author) + ")", icon_url = ctx.author.avatar_url)
+    await judgement_message.edit(content="", embed=embed)
 
-        jury.log(ctx.message.author.id, judgement)
-        await ctx.channel.send(message)
-        
 @bot.command()
-async def view(ctx: commands.Context, question_number: str) -> None:
+async def tests(ctx: commands.Context, question_number: str=None) -> None:
+    '''Sends the given question's test input and answer
+    '''
+    if not question_number or not check_valid_question_number_string(question_number):
+        await ctx.channel.send("What question do you want to view? (q1, 2, q3, etc.)")
+        question_number = await bot.wait_for("message", check=check_valid_question_number_message)
+        question_number = question_number.content
+    question_number = int(question_number.lstrip('q')) - 1
+    question = jury.questions[question_number]
+
+    files = []
+    question_folder_path = f"questions/{question['name']}"
+    input_file = discord.File(question_folder_path + "/INPUT.txt", filename=f"Q{question_number+1} INPUT.txt")  # Display 1 indexed number
+    answer_file = discord.File(question_folder_path + "/ANSWER.txt", filename=f"Q{question_number+1} ANSWER.txt")  # Display 1 indexed number
+    files.extend((input_file, answer_file))
+
+    judgement = jury.get_judgement(get_identifier(ctx.author), question_number)
+    if judgement:
+        output_file = BytesIO(bytes(judgement["output"], encoding="utf-8"))
+        output_file = discord.File(output_file, filename=f"Q{question_number+1} {format_time(judgement['submission_time'])} OUTPUT.txt")
+        files.append(output_file)
+
+    status = "Not submitted" if not judgement else ("Passed" if judgement["passed"] else "Failed")
+    message = f"Test data for question Q{question_number+1} {question['name']} ({status})"
+    await ctx.channel.send(content=message, files=files)
+
+
+@bot.command()
+async def view(ctx: commands.Context, question_number: str=None) -> None:
     '''Displays the given question.
     '''
-    if not check_valid_question_number_string(question_number):
+    if not question_number or not check_valid_question_number_string(question_number):
         await ctx.channel.send("What question do you want to view? (q1, 2, q3, etc.)")
         question_number = await bot.wait_for("message", check=check_valid_question_number_message)
         question_number = question_number.content
     question_number = int(question_number.lstrip('q'))
     question = jury.questions[question_number-1]
     question_prompt = question["question"]
-    question_prompt = f"**Q{question_number}. {question['name']}**\n" + question_prompt
-    await ctx.channel.send(question_prompt)
+    embed = discord.Embed(title=f"Q{question_number}. {question['name']}", description=question_prompt)
+    embed.set_footer(
+            text="Requested by " + ctx.author.display_name + " (" + get_identifier(ctx.author) + ")", icon_url = ctx.author.avatar_url)
+    await ctx.channel.send(embed=embed)
+
+
+# MODERATION COMMANDS
+@bot.command(hidden=True)
+@commands.has_permissions(administrator=True)
+async def delete(ctx: commands.context, team, question_number):
+    '''Deletes a team's latest submission for a certain question.
+
+    Args:
+        team: Either the team number (e.g. 1, 2) or an ID of one of the team members.
+        question_number: The question number the submission is to be disqualified for.
+    '''
+    if len(team) > 20:
+        try:
+            user = ctx.message.server.get_member(int(team))
+            team = get_identifier(user)
+        except BaseException as e:
+            ctx.channel.send(f"Couldn't find user's team: {e}")
+            return
+
+    judgement = get_judgement(team, question_number)
+    if not judgement:
+        ctx.channel.send(f"Team {team} has not submission for question {question_number}")
+        return
+
+    attempt_time = get_format(judgement["submission_time"])
+    ctx.channel.send(f"Do you want to delete Team {team}'s submission for {question_number} at {attempt_time}?")
+    message = bot.wait_for("message", check=lambda msg: msg.content.lower() in ("yes", "no"))
+    if message.content.lower() == "yes":
+        jury.judgements[team].pop(question_number)
+        ctx.channel.send("Question judgement removed.")
+    else:
+        ctx.channel.send("Command cancelled.")
+
+@bot.command(hidden=True)
+@commands.has_permissions(administrator=True)
+async def freeze(ctx: commands.context):
+    '''Freezes information about the competition into a file.
+    '''
+    time = ctx.message.created_at.replace(tzinfo=pytz.timezone("UTC")).astimezone(pytz.timezone('Asia/Kolkata'))
+    if not os.path.exists("LOGS"):
+        os.mkdir("LOGS")
+
+    with open("LOGS/"+time.strftime("%I_%M")+".dat", "wb") as log:
+        pickle.dump(jury.judgements, log)
+    await ctx.channel.send("Logs dumped.")
+
+# INITIALIZATION
+@bot.listen("on_message")
+async def intialize_teams(message):
+    for role in message.guild.roles:
+        if role.name not in jury.judgements and role.name.lower() in ('lotus valley', 'dav public school', 'heritage school', 'gd goenka', 'shalom hills', 'kunskapsskolan', 'dps'):
+            jury.judgements[role.name] = {}
+            print("Added", role.name)
 
 bot.run(TOKEN)
--- jury.py
+++ jury.py
@@ -1,9 +1,10 @@
-import json
 import difflib
+import json
+from datetime import datetime, timedelta
+from typing import Dict
 from Tio import Tio
 
 questions = []
-participants = []
 judgements = {}
 
 # LOAD IN QUESTIONS
@@ -16,80 +17,131 @@ with open("questions/info.json") as info:
 			question_data["name"] = question_name
 			question_data["number"] = len(questions)  # 0 indexed
 			question_data["level"] = level
-			question_data["multiplier"] = info["multipliers"][level]
-			question_data["submissions"] = {}
-			with open(f"questions/{question_name}/QUESTION.md", "r") as file:
+			question_data["penalty"] = info["penalties"][level]
+			question_data["points"] = {}
+			with open(f"questions/{question_name}/QUESTION.md", "r", encoding="UTF-8") as file:
 				question_data["question"] = file.read()
-			with open(f"questions/{question_name}/INPUT.txt", "r") as file:
+			with open(f"questions/{question_name}/INPUT.txt", "r", encoding="UTF-8") as file:
 				question_data["input"] = file.read()
-			with open(f"questions/{question_name}/ANSWER.txt", "r") as file:
+			with open(f"questions/{question_name}/ANSWER.txt", "r", encoding="UTF-8") as file:
 				question_data["answer"] = file.read()
 			questions.append(question_data)
 
-def get_leaderboard(include_times=True):
-	'''Sorts the participants by their submissions.'''
+TIME_BEGIN = datetime.now().astimezone()
+DURATION = timedelta(minutes=120)
+
+def P_speed(submission_time: datetime) -> float:
+	time_passed = submission_time - TIME_BEGIN
+	points = 250 * (1 - time_passed/DURATION)
+	return points
+
+def P_efficiency(execution_time: int, question_number: int) -> float:
+	least_execution_time = get_least_execution_time(question_number) or execution_time
+	points = 250 * (least_execution_time/execution_time)
+	return points
+
+def P_total(question_number: int, submission_time: datetime, execution_time: int) -> float:
+	points = P_speed(submission_time) + P_efficiency(execution_time, question_number)
+	points = round(points, 2)
+	return points
+
+def get_least_execution_time(question_number: int) -> int:
+	question = questions[question_number]
+	least_execution_time = None
+	for team in question["points"]:
+		question_judgement = judgements[team][question_number]
+		execution_time = question_judgement["least_execution_time"]
+		if not least_execution_time or execution_time < least_execution_time:
+			least_execution_time = execution_time
+	return least_execution_time
+
+def get_penalised_points(question_number: int) -> float:
+	question = questions[question_number]
+	points = question["points"].values()
+	if len(points) == 0:
+		return 0
+	else:
+		least_points = min(points)
+		penalised = round(least_points - question["penalty"], 2)
+		return max(0, penalised)
+
+def get_leaderboard(include_points=True):
+	'''Sorts the participants by their net points.'''
 	global questions
-	total_time = {user_id: 0 for user_id in participants}
-	slowest_times = {}
-	for user_id in participants:
-		for question in questions:
-			submissions = question["submissions"]
-			if user_id in submissions:
-				total_time[user_id] += submissions[user_id]
+	total_points = {team: 0 for team in judgements.keys()}
+
+	for question in questions:
+		# Record the slowest time for this question
+		if len(question["points"]) == 0:
+			# No one submitted, skip the question altogether
+			continue
+
+		for team in judgements.keys():
+			if team in question["points"]:
+				# Give them their points
+				total_points[team] += question["points"][team]
 			else:
-				if question["number"] not in slowest_times:
-					slowest_times[question["number"]] = min( list(submissions.values()) + [0])  # Add 0 in case no one submitted
-				total_time[user_id] += slowest_times[question["number"]] * question["multiplier"]
+				# They get the penalised points
+				total_points[team] += get_penalised_points(question["number"])
 
 	# Flatten and sort
-	total_time = [{"id": user_id, "time": total_time[user_id]} for user_id in total_time]
-	total_time.sort(key=lambda p_data: p_data["time"])  # Sort by total time
-	if not include_times:
-		total_time = [p_data["id"] for p_data in total_time]  # Get the participant ids
-	return total_time 
+	total_points = [{"team": team, "points": total_points[team]} for team in total_points]
+	print(total_points)
+	total_points.sort(key=lambda t: round(t["points"], 2), reverse=True)  # Sort by total points
+	if not include_points:
+		total_points = [t["team"] for t in total_points]  # Give a list of the teams only
+	print("\t", total_points)
+	return total_points
 
-class Diff:
-	def __init__(self, correct, wrong):
-		correct_lines, wrong_lines = correct.split("\n"), wrong.split("\n")
-		self.unified = difflib.unified_diff(
-			a=[line + "\n" for line in correct_lines], b=[line + "\n" for line in wrong_lines],
-			fromfile="Answer", tofile="Output"
-		)
-		self.unified = "".join(self.unified)  # unified_diff gives lines, each with the \n added
-		import sys
-		sys.stdout.writelines(self.unified)
-
-		sequence = difflib.SequenceMatcher(None, correct_lines, wrong_lines)
-		first_correct_lines = sequence.get_matching_blocks()[0].size
-		self.flod = first_correct_lines + 1
-		pass
-
-class Judgement:
-	def __init__(self, question, response):
-		self.output = response.stdout
-		self.passed = self.output.strip() == question["answer"].strip()
-		self.question = question
-		self.time_taken = float(response.real_time)
-		self.diff = Diff(question["answer"], self.output)
+def get_first_line_of_difference(correct, wrong):
+	correct_lines, wrong_lines = correct.split("\n"), wrong.split("\n")
+	sequence = difflib.SequenceMatcher(None, correct_lines, wrong_lines)
+	first_correct_lines = sequence.get_matching_blocks()[0].size
+	return first_correct_lines + 1
 
 site = Tio()
-def judge(question_number: int, language: str, source_code: str) -> Judgement:
+def judge(question_number: int, language: str, source_code: str, submission_time: datetime) -> Dict[str, any]:
 	question = questions[question_number]
 	request = site.new_request(language, source_code, question["input"])
 	response = site.send(request)
-	judgement = Judgement(question, response)
+	output = response.stdout
+	passed = output.strip() == question["answer"].strip()
+	execution_time = float(response.real_time)
+	judgement = {
+		"question_number": question_number,
+		"code": source_code,
+		"output": output,
+		"passed": passed,
+		"execution_time": execution_time,
+		"submission_time": submission_time,
+	}
+	if not passed:
+		# Diff solving
+		correct_lines, wrong_lines = question["answer"].split("\n"), output.split("\n")
+		sequence = difflib.SequenceMatcher(None, correct_lines, wrong_lines)
+		lod = sequence.get_matching_blocks()[0].size + 1
+		diff = {
+			"lod": lod,
+			"correct": correct_lines[lod-1],
+			"wrong": wrong_lines[lod-1]
+		}
+		judgement["diff"] = diff
+	if passed:
+		judgement["points"] = P_total(question["number"], submission_time, execution_time)
+	else:
+		judgement["points"] = "N.A."
 	return judgement
 
-def log(user_id: int, judgement: Judgement):
-	if user_id not in participants:
-		participants.append(user_id)
-		judgements[user_id] = {}
-	judgements[user_id][judgement.question["number"]] = judgement
-	if judgement.passed:
-		questions[judgement.question["number"]]["submissions"][user_id] = judgement.time_taken
+def log(team: str, judgement: Dict[str, any]):
+	if team not in judgements:
+		judgements[team] = {}
+	judgements[team][judgement["question_number"]] = judgement
+	if judgement["passed"]:
+		questions[judgement["question_number"]]["points"][team] = judgement["points"]
 
-def get_judgement(user_id: int, question_number: int):
-	if user_id in judgements and question_number in judgements[user_id]:
-		return judgements[user_id][question_number]
+def get_judgement(team: str, question_number: int):
+	print(judgements.keys())
+	if team in judgements and question_number in judgements[team]:
+		return judgements[team][question_number]
 	else:
 		return None
